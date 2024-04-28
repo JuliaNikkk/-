cf5b52b4601d5f0b9288226352ea370125822b90
AlabamaWebp <dimyrp@yandex.ru>
2024-04-28 23:03:34 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,27 +1,37 @@
 
 function checkColor() {
-    const tmp = document.getElementsByClassName(`existance`);
-    for (let i = 0; i < tmp.length; i++) {
+    const tmp = document.getElementsByClassName("existence")
+    for (let i=0; i < tmp.length; i++){
         const el = tmp[i];
-        const txt = el.innerText;
-        if (txt == 'В наличии') {
-            el.classList.add('green');
+        const txt = el.innerText
+        if (txt == 'В наличии'){
+            el.classList.add('green2');
         }
-        else if (txt == 'Нет в наличии') el.classList.add('red');
+        else if (txt == 'Нет в наличии'){
+            el.classList.add ('red1');
+        }
     }
-    // tmp.forEach(el => {
-    //     const txt = el.innerText;
-    //     if (txt == 'В наличии') el.classList.add('green');
-    //     else if (txt == 'Нет в наличии') el.classList.add('red');
-    // });
+
 }
 checkColor();
 
-let bublik = 'Бубли'
-for (let i = 10; i > 7; i--) {
-    if (bublik == "Бублик")
-        console.log("АААААААААААА");
-    else
-        console.log(bublik);
-    bublik = bublik + 'к'
+
+function txtChange(el){
+    const txt = el.innerText
+if (txt == 'Добавить в корзину'){
+    el.innerText = 'В корзине';
+}    
+else if (txt == 'В корзине'){
+    el.innerText = 'Добавить в корзину'
 }
+
+    // if ()
+}
+// let bublik = 'Бубли'
+// for (let i = 10; i > 7; i--) {
+//     if (bublik == "Бублик")
+//         console.log("АААААААААААА");
+//     else
+//         console.log(bublik);
+//     bublik = bublik + 'к'
+// }
